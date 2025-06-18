8a3e0598974384e24697b220e1c979b60a88e4b4
Aditya17-ai <130999483+Aditya17-ai@users.noreply.github.com>
2025-06-18 21:08:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import connectDb from './db.js';
 import { Server } from 'socket.io';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const allowedOrigins = [
   'mini-crm-tau.vercel.app', // replace with your actual frontend Render URL
